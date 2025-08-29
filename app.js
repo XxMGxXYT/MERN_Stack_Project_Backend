a4ff05be0f7f5f16e98c2167e0100c095c23c7fe
XxMGxX IT/AI <mohamad7719603@gmail.com>
2025-08-29 03:38:48 +0300

    Up

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import connectDB from './config/db.js';
 import productsRoute from './routes/productsRoute.js';
 import path from 'path';
 import cors from 'cors';
-import homeRoute from './routes/homeRoute.js';
+import { getAllProductsHandler, createProductHandler, updateProductHandler, deleteProductHandler } from "./controllers/productsControllers.js";
 const app = express();
 const port = process.env.PORT || 2003;
 
@@ -22,9 +22,16 @@ app.use(express.static('public')); // Serve static files from the 'public' direc
 app.use(express.json()); // Middleware to parse JSON bodies
 
 // Serve the index.html file for the root route
-app.use("/", homeRoute)
+app.get("/", (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'public', 'index.html'));
+});
 
-app.use("/api/products", productsRoute)
+
+app.get("/api/products/", getAllProductsHandler)
+app.post("/api/products/", createProductHandler)
+app.put("/api/products/:id", updateProductHandler)
+app.delete("/api/products/:id", deleteProductHandler);
+
 
 
 // Connect to MongoDB
